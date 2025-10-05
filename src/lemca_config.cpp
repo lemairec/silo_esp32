8af23b2ae0c74fc1d74aa1a5e1b8dd72e07ebb9f
Clement Lemaire <lemairec@macbook-pro-de-clement.home>
2025-10-05 22:11:00 +0200

    Config

---
+++ src/lemca_config.cpp
@@ -0,0 +1,11 @@
+#include "lemca_config.h"
+
+#include "common/util.h"
+
+#include <string>
+
+#include <stdio.h>
+
+void printConfig(){
+    lc_DebugPrint("*** config")
+}
---
+++ src/lemca_config.h
@@ -0,0 +1,16 @@
+#ifndef LEMCA_CONFIG_H_
+#define LEMCA_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define OUTIL_NAME "SONDE"
+
+extern void printConfig();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
